b411ea0abcd45356d07ec9fca454ae5d0ebe4623
sagar <singour.sagar@gmail.com>
2025-02-28 18:18:30 +0530

    Added logic to pass test for negative numbers not allowed

--- src/add.ts
+++ src/add.ts
@@ -12,7 +12,20 @@ const add = (numbers: string): number => {
   }
 
   const numberArr = numbersToSum.split(delimiter);
-  const sum = numberArr.reduce((acc, num) => acc + parseInt(num, 10), 0);
+  const negativeNumbers: string[] = [];
+
+  const sum = numberArr.reduce((acc, num) => {
+    const numInt = parseInt(num, 10);
+    if (numInt < 0) {
+      negativeNumbers.push(num);
+      return acc;
+    }
+    return acc + parseInt(num, 10);
+  }, 0);
+
+  if (negativeNumbers.length > 0) {
+    throw new Error(`Negatives not allowed: ${negativeNumbers.join(",")}`);
+  }
   return sum;
 };
 
