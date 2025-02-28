0ec428b880ff3eb66de6126f716385d3f49c425e
sagar <singour.sagar@gmail.com>
2025-02-28 12:36:29 +0530

    Fail test for '1'

--- src/add.test.ts
+++ src/add.test.ts
@@ -5,4 +5,8 @@ describe('add() should calculate numbers', () => {
   it('Should return 0 for empty string', () => {
     expect(add("")).toEqual(0)
   })
+
+  it('Should return 1 for "1"', () => {
+    expect(add("1")).toEqual(1)
+  })
 })
