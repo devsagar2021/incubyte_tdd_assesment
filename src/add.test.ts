32d8f9c839dc8ffa676147467d38e27414eb8730
sagar <singour.sagar@gmail.com>
2025-02-28 12:41:05 +0530

    Fail test for '1,5'

--- src/add.test.ts
+++ src/add.test.ts
@@ -9,4 +9,8 @@ describe('add() should calculate numbers', () => {
   it('Should return 1 for "1"', () => {
     expect(add("1")).toEqual(1)
   })
+
+  it('Should return 6 for "1,5"', () => {
+    expect(add("1,5")).toEqual(6)
+  })
 })
