9b9ffa2a46285e55eb063de548641bf20e88c00e
sagar <singour.sagar@gmail.com>
2025-02-28 12:51:48 +0530

    Same logic is passed test for '5,2,10,1,2'

--- src/add.test.ts
+++ src/add.test.ts
@@ -13,4 +13,8 @@ describe('add() should calculate numbers', () => {
   it('Should return 6 for "1,5"', () => {
     expect(add("1,5")).toEqual(6)
   })
+
+  it('Should return 10 fro "5,2,10,1,2"', () => {
+    expect(add("5,2,10,1,2")).toEqual(20)
+  })
 })
